69ebd50f0382fe1570a63d0353cd2d7eadb65a58
wangzp0828 <111725348+wangzp0828@users.noreply.github.com>
2022-08-23 14:12:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 import os
 import random
 
-today = datetime.now()
+today = datetime.now().strftime("%m-%d")
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
