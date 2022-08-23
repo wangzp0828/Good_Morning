7b1a462c2398cb84a4ef138b5af2dab99feb9b67
wangzp0828 <111725348+wangzp0828@users.noreply.github.com>
2022-08-23 15:52:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_back():
     return "距离上次回来已经过去%d天" % (today-next).days
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  next = datetime.strptime(str(date.today().year) + "-" + birthday +' 23:59:59', "%Y-%m-%d %H:%M:%S")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
